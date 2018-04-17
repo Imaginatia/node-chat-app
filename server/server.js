180d5aa10d36edd432ced729fa003cf2e5fc2e64
Zhenye <492684811@qq.com>
2018-04-16 23:47:10 -0400

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const express = require('express');
+const fs = require('fs');
+var app = express();
+
+const publicPath = path.join(__dirname, '../public');
+
+console.log(publicPath);
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
